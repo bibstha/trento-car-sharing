80f996197b1e46640e44f13fa8c4652a62835501
Bibek Shrestha <bibek@Bibeks-MacBook-Air.local>
2013-11-09 13:31:44 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TrentoCarSharing::Application.config.secret_key_base = '734cd261c5f7764ebd73e08f0bc636f3bd9e321ef66f14251c389fa2d3bc314491e8b19dd38af65d9a47139b3d238ccbd46ea45d462ac1be2f8842331b729e64'
