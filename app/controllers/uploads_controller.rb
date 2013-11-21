e07af8eb2594e9cc4342d6ec16c9f3b3e0eb8aab
Bibek Shrestha <bibekshrestha@gmail.com>
2013-11-21 20:57:29 +0100

    Fixed directory and capistrano shared folder

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -3,13 +3,13 @@ require 'fileutils'
 class UploadsController < ApplicationController
   def create
     uploaded_io = params[:file]
-    upload_path = Rails.root.join('public', 'uploads', uploaded_io.original_filename)
+    upload_path = Rails.root.join('public', 'system', 'uploads', uploaded_io.original_filename)
     orig_path = upload_path
     i = 0
     while upload_path.exist?
       i = i + 1
       new_name = "#{orig_path.basename(".*")}_#{i}#{upload_path.extname}"
-      upload_path = Rails.root.join('public', 'uploads', new_name)
+      upload_path = Rails.root.join('public', 'system', 'uploads', new_name)
     end
     FileUtils.mkdir_p upload_path.dirname unless upload_path.dirname.exist?
     File.open(upload_path, 'wb') do |file|
